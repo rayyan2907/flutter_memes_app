6d1daa767eaf7a6c4b4d1355cd5d03a6136d87ca
rayyan2907 <mrayyan5296@gmail.com>
2025-06-21 22:38:21 +0500

    splash screen done

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -7,8 +7,13 @@ class splash_screen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-           
+          Text("My Meme App",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50, color: Color.fromRGBO(0, 200, 200, 1)),),
+          SizedBox(height: 40,),
+          Image.network("https://cdn.i-scmp.com/sites/default/files/styles/1020x680/public/d8/images/canvas/2021/10/30/85f2cb5f-44f8-4f2f-a813-63e657e11acc_5065cac7.jpg?itok=dugQzfiu&v=1635566576"),
+          SizedBox(height: 20,),
+          Text("MEME APP",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
         ],
       ),)
     );
