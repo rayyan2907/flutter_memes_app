8e6f2a8319a7fd00ef0806dd97299914d9f4ba07
rayyan2907 <mrayyan5296@gmail.com>
2025-06-21 21:55:32 +0500

    starting making splash screen

---
+++ lib/view/main_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class main_screen extends StatelessWidget {
+  const main_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class splash_screen extends StatelessWidget {
+  const splash_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Column(
+        children: [
+           
+        ],
+      ),)
+    );
+  }
+}
